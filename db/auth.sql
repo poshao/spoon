fe9688284ae878631ac3c819d8e320316dfab527
Byron Gong <byron.gong@ap.averydennison.com>
2018-12-12 13:59:02 +0800

    更新用户管理表

---
+++ db/auth.sql
@@ -0,0 +1,106 @@
+set names utf8;
+
+drop database if exists `SpoonTest`;
+create database `SpoonTest` default charset utf8 collate utf8_general_ci;
+
+use `SpoonTest`;
+
+#用户认证数据库
+
+#创建用户表
+drop table if exists `auth_users`;
+create table `auth_users`(
+    `id` int(11) primary key auto_increment not null,
+    `psid` int(11) not null comment '全球员工号',
+    `workid` int(11) not null comment '工号',
+    `username_en` varchar(30) comment '英文名字',
+    `username_zh` varchar(30) comment '中文名字',
+    `password` varchar(64) not null comment '密码',
+    `depart` varchar(30) comment '部门',
+    `status` varchar(10) not null default 'unaudited' comment '用户状态',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_users` ADD UNIQUE INDEX `psid_unique` (`psid` ASC);
+ALTER TABLE `auth_users` ADD UNIQUE INDEX `workid_unique` (`workid` ASC);
+
+#创建角色表
+drop table if exists `auth_roles`;
+create table `auth_roles`(
+    `id` int(11) primary key auto_increment not null,
+    `rolename` varchar(50) not null comment '角色名称',
+    `description` varchar(100) comment '描述',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_roles` ADD UNIQUE INDEX `rolename_unique`(`rolename` ASC);
+
+#创建分组表
+drop table if exists `auth_groups`;
+create table `auth_groups`(
+    `id` int(11) primary key auto_increment not null,
+    `groupname` varchar(60) not null comment '分组名称',
+    `description` varchar(100) comment '描述',
+    `roleid` int(11) not null default 0 comment '默认角色',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_groups` ADD UNIQUE INDEX `groupname_unique`(`groupname` ASC);
+
+#创建权限表
+drop table if exists `auth_permissions`;
+create table `auth_permissions`(
+    `id` int(11) primary key auto_increment not null,
+    `permissionname` varchar(50) not null comment '权限名称',
+    `url` varchar(100) not null comment '权限代码',
+    `description` varchar(100) comment '描述',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_permissions` ADD UNIQUE INDEX `permissionname_unique`(`permissionname` ASC);
+
+#创建登录状态表
+drop table if exists `auth_sessions`;
+create table `auth_sessions`(
+    `id` int(11) primary key auto_increment not null,
+    `userid` int(11) not null comment '用户ID',
+    `token` varchar(64) not null comment '用户密钥',
+    `ip` varchar(20) comment 'ip地址',
+    `valid_time` datetime not null comment '有效时间',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_sessions` ADD UNIQUE INDEX `userid_unique`(`userid` ASC);
+
+#创建用户角色关联表
+drop table if exists `auth_ref_user_role`;
+create table `auth_ref_user_role`(
+    `id` int(11) primary key auto_increment not null,
+    `userid` int(11) not null comment '用户ID',
+    `roleid` int(11) not null comment '角色ID',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_ref_user_role` ADD UNIQUE INDEX `userrole_unique`(`userid` ASC,`roleid` ASC);
+
+#创建角色权限关联表
+drop table if exists `auth_ref_role_permission`;
+create table `auth_ref_role_permission`(
+    `id` int(11) primary key auto_increment not null,
+    `roleid` int(11) not null comment '角色ID',
+    `permissionid` int(11) not null comment '权限ID',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_ref_role_permission`ADD UNIQUE INDEX `rolepermission_unique`(`roleid` ASC,`permissionid` ASC);
+
+#创建用户分组关联表
+drop table if exists `auth_ref_user_group`;
+create table `auth_ref_user_group`(
+    `id` int(11) primary key auto_increment not null,
+    `userid` int(11) not null comment '用户ID',
+    `groupid` int(11) not null comment '分组ID',
+    `create_time` datetime not null default CURRENT_TIMESTAMP,
+    `update_time` datetime not null default CURRENT_TIMESTAMP
+)engine=innodb;
+ALTER TABLE `auth_ref_user_group` ADD UNIQUE INDEX `usergroup_unique`(`userid` ASC,`groupid` ASC);
