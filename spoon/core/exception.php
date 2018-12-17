c5f799ed2268c32e7c9d5434683182898851104e
Byron Gong <byron.gong@ap.averydennison.com>
2018-12-17 18:32:45 +0800

    增加错误提示

--- spoon/core/exception.php
+++ spoon/core/exception.php
@@ -5,13 +5,25 @@
 namespace Spoon;
 
 class Exception extends \Exception{
+    
+    // public function __construct($msg="",$code=0){
+    //     $this->message=$msg;
+    //     $this->code=$code;
+    // }
     /**
      * 渲染错误信息
      *
      * @return void
      */
-    function render(){
-        Response::send(json_encode(array('error'=>$this->getMessage().' (line:'.$this->getLine().' code:'.$this->getCode().')')),500);
+    public function render(){
+        Response::send(json_encode(array(
+            'error'=>array(
+                'message'=>$this->getMessage(),
+                'code'=>$this->getCode(),
+                'file'=>$this->getFile(),
+                'line'=>$this->getLine()
+            )
+        )),$this->getCode()>400?$this->getCode():500);
         \Spoon\Logger::error($this->__toString());
     }
 }
