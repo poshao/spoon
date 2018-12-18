2d4b09e44a2af38a304586f1ed6aad1f96a784ff
Byron Gong <byron.gong@ap.averydennison.com>
2018-12-18 17:39:54 +0800

    区分调试模式

--- spoon/core/exception.php
+++ spoon/core/exception.php
@@ -6,24 +6,28 @@ namespace Spoon;
 
 class Exception extends \Exception{
     
-    // public function __construct($msg="",$code=0){
-    //     $this->message=$msg;
-    //     $this->code=$code;
-    // }
     /**
      * 渲染错误信息
      *
      * @return void
      */
     public function render(){
-        Response::send(json_encode(array(
-            'error'=>array(
-                'message'=>$this->getMessage(),
-                'code'=>$this->getCode(),
-                'file'=>$this->getFile(),
-                'line'=>$this->getLine()
-            )
-        )),$this->getCode()>400?$this->getCode():500);
+        $debug=\Spoon\Config::get('debug');
+
+        if($debug){
+            $data=array(
+                'error'=>array(
+                    'message'=>$this->getMessage(),
+                    'code'=>$this->getCode(),
+                    'file'=>$this->getFile(),
+                    'line'=>$this->getLine()
+                )
+            );
+        }else{
+            $data=array('error'=>$this->getMessage());
+        }
+
+        Response::send(json_encode($data),$this->getCode()>=400?$this->getCode():500);
         \Spoon\Logger::error($this->__toString());
     }
 }
