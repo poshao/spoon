4ad18e6307dd9f9403d8e7dfdced35e73f74e10d
Byron Gong <byron.gong@ap.averydennison.com>
2019-01-03 11:46:26 +0800

    增加依赖注入,主要实现权限验证

---
+++ spoon/core/di.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 依赖注入管理
+ */
+namespace Spoon;
+class DI{
+    protected static $_services=null;
+
+    public static function getDI($name){
+        return self::$_services[$name];
+    }
+
+    public static function setDI($name,$obj){
+        self::$_services[$name]=$obj;
+    }
+}
+?>
